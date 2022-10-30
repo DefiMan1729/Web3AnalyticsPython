8808e00b94288037456956678010de1d58cf0dad
DeFiMan1729.eth <115624087+DefiMan1729@users.noreply.github.com>
2022-10-30 10:16:13 +0530

    extra nodes added

--- Web3Analytics.py
+++ Web3Analytics.py
@@ -8,7 +8,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+# Blockchain RPC URL
 blockchain_address = os.getenv("GANACHE_URL")
 
 # Client instance to interact with the blockchain
@@ -23,18 +23,19 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 G=nx.DiGraph()
 
 #I used the local contract JSON ABI path 
+#you can also paste the ABI directly. Copying the ABI is very easy if you are using Remix
+#contract_abi='<ABI>'
 compiled_contract_path = os.getenv("ABI_JSON_PATH")
 with open(compiled_contract_path) as file:
     contract_json = json.load(file)  
     contract_abi = contract_json['abi']  
 
-#you can also paste the ABI directly. Copying the ABI is very easy if you are using Remix
-#contract_abi='<ABI>'
 
+#I deployed an Openzeppelin NFT (ERC721) contract locally 
 deployed_contract_address = Web3.toChecksumAddress(os.getenv("CONTRACT_ADDRESS"))
-# deployed_contract_address = Web3.toChecksumAddress(0xd154b3bEB9A1461DcB529DbF49E77d7ccD6521F9)
 
-# Call function (this is not persisted to the blockchain)
+
+# Call function (this is not persisted to the blockchain) to get the Token name and Symbol 
 contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi)
 message1 = contract.functions.name().call()
 print(message1)
@@ -46,6 +47,7 @@ tx_hs = contract.functions.safeMint(web3.eth.accounts[0],'defiman1729.eth.limo')
 receipt = web3.eth.get_transaction_receipt(tx_hs)
 # print(receipt)
 
+#store the Transfer event data (From, To and tokenId) in local variable
 _to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
 print(_to)
 _from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
@@ -53,60 +55,96 @@ print(_from)
 _tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
 print(_tokenid)
 
+#form the graph nodes using the variables
 G.add_node(_from)
 G.add_node(_to)
 G.add_edge(_from,_to)
 
-
+# -------------------------
 #transfer token from default address accounts[0]  accounts[1] to 
 tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[0],web3.eth.accounts[1],_tokenid).transact()
 receipt = web3.eth.get_transaction_receipt(tx_hs)
-print(receipt)
+# print(receipt)
 
 _to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
-print(_to)
+# print(_to)
 _from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
-print(_from)
+# print(_from)
 _tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
-print(_tokenid)
+# print(_tokenid)
 
 G.add_node(_from)
 G.add_node(_to)
 G.add_edge(_from,_to)
 
 
-
+#set the default address to web3.eth.accounts[1]
 web3.eth.defaultAccount = web3.eth.accounts[1]
+
 #transfer token from default address accounts[1]  accounts[2] to 
 tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[1],web3.eth.accounts[2],_tokenid).transact()
 receipt = web3.eth.get_transaction_receipt(tx_hs)
-print(receipt)
+
 
 _to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
-print(_to)
+
 _from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
-print(_from)
+
 _tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
-print(_tokenid)
+
+
+G.add_node(_from)
+G.add_node(_to)
+G.add_edge(_from,_to)
+
+#set the default address to web3.eth.accounts[2]
+web3.eth.defaultAccount = web3.eth.accounts[2]
+
+#transfer token from default address accounts[2]  accounts[0] to 
+tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[2],web3.eth.accounts[0],_tokenid).transact()
+receipt = web3.eth.get_transaction_receipt(tx_hs)
+
+
+_to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
+
+_from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
+
+_tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
+
 
 G.add_node(_from)
 G.add_node(_to)
 G.add_edge(_from,_to)
 
 
-web3.eth.defaultAccount = web3.eth.accounts[2]
-#transfer token from default address accounts[2]  accounts[0] to 
-tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[2],web3.eth.accounts[0],_tokenid).transact()
+#set the default address to web3.eth.accounts[0]
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+#transfer token from default address accounts[0]  accounts[3] to 
+tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[0],web3.eth.accounts[3],_tokenid).transact()
 receipt = web3.eth.get_transaction_receipt(tx_hs)
-print(receipt)
 
 _to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
-print(_to)
-_from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
-print(_from)
-_tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
-print(_tokenid)
 
+_from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
+
+_tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
+
+
+G.add_node(_from)
+G.add_node(_to)
+G.add_edge(_from,_to)
+
+#set the default address to web3.eth.accounts[3]
+web3.eth.defaultAccount = web3.eth.accounts[3]
+
+#transfer token from default address accounts[3]  accounts[0] to 
+tx_hs = contract.functions.safeTransferFrom(web3.eth.accounts[3],web3.eth.accounts[0],_tokenid).transact()
+receipt = web3.eth.get_transaction_receipt(tx_hs)
+
+_to=contract.events.Transfer().processReceipt(receipt)[0]['args']['to']
+_from=contract.events.Transfer().processReceipt(receipt)[0]['args']['from']
+_tokenid=contract.events.Transfer().processReceipt(receipt)[0]['args']['tokenId']
 G.add_node(_from)
 G.add_node(_to)
 G.add_edge(_from,_to)
